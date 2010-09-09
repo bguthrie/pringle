b46f7b3facb01f345af79236f7a8e712f66dd355
Brian & Usha <viewpoints_tw@thoughtworks.com>
2010-09-08 23:42:12 -0400

    Added jQuery dataset support. Added story owner, type, and blocked status. Some formatting changes.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -5,10 +5,6 @@ Mingle = {
       params = {};
     }
 
-    if (typeof fn === "undefined") {
-      fn = function(data) { console.log(data); };
-    }
-
     $.get("/mingle", { path: path, params: $.param(params || {}) }, fn, "json");
   },
 
@@ -17,10 +13,14 @@ Mingle = {
     $(card.properties).each(function() {
       if (this.name === propName) { property = this; }
     });
-
+    if (property) { console.log(property.value); }
     return property ? property.value : null;
   }
 }
+
+$.fn.swimlane = function(laneName) {
+  return $(this).find("tbody .swimLane").filter("[data-name='" + laneName + "']");
+};
 
 $(document).ready(function() {
   $("#status").html("Loading story wall...");
@@ -28,24 +28,26 @@ $(document).ready(function() {
   storyWall.hide();
   var CardColors = {};
 
-  // var row  = function(content) { return $("<tr></tr>").append(content); }
-  // var cell = function(content) { return $(content).appendTo("<td></td>").closest("td"); }
-
   var initCards = function() {
     Mingle.get("/projects/agile_hybrid/cards", { page: "all" }, function(data) {
       var template = $( $("#cardTemplate").html() );
 
       $(data.cards).each(function() {
-        console.log(this);
         var status = Mingle.property(this, "Story Status") || Mingle.property(this, "Defect Status") || Mingle.property(this, "Risk Status") || Mingle.property(this, "Feature Status");
-        var swimlane = storyWall.find("tbody .swimLane[data-name='" + status + "']");
 
         var card = template.clone();
         card.attr("data-type", this.card_type.name);
         card.css({ backgroundColor: CardColors[this.card_type.name] });
         card.find(".number").html("#" + this.number);
         card.find(".name").html(this.name);
-        $(swimlane).append(card);
+        card.find(".type").html(this.card_type.name);
+        card.find(".owner").html( (Mingle.property(this, "Owner") || {}).name );
+
+        if (Mingle.property(this, "Blocked") === "Yes") {
+          card.addClass("blocked");
+        }
+
+        storyWall.swimlane(status).append(card);
       });
 
       $("#status").html("");
@@ -67,17 +69,14 @@ $(document).ready(function() {
     var template = $( $("#swimlaneTemplate").html() );
 
     var headers = $(data.property_definition.property_value_details).map(function() {
-      return $("<th/>").addClass("swimLane").html(this.value).attr("data-name", this.value);
+      return $("<th/>").addClass("swimLane").html(this.value).dataset({name: this.value});
     });
 
     var emptyCols = $(data.property_definition.property_value_details).map(function() {
-      return $("<td>&nbsp;</td>").addClass("swimLane").attr("data-name", this.value);
+      return $("<td>&nbsp;</td>").addClass("swimLane").dataset({name: this.value});
     });
 
-    $("<tr/>").appendTo(storyWall.find("thead"));
     $(headers).appendTo(storyWall.find("thead tr"));
-
-    $("<tr/>").appendTo(storyWall.find("tbody"));
     $(emptyCols).appendTo(storyWall.find("tbody tr"));
 
     initCardColors();
