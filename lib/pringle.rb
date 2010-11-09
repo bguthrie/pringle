f08e1816235a325606388cdc65fcbdb46e86ba74
Brian Guthrie <btguthrie@gmail.com>
2010-11-08 22:26:50 -0500

    Made Mingle server configuration easier to change.

--- lib/pringle.rb
+++ lib/pringle.rb
@@ -4,10 +4,15 @@ require 'wrest'
 require 'haml'
 require 'active_support/json/encoding'
 
+%w(MINGLE_USERNAME MINGLE_PASSWORD MINGLE_HOST).each do |configuration_setting|
+  unless ENV.has_key?(configuration_setting)
+    raise "Configuration setting #{configuration_setting} not available. Exiting."
+  end
+  
+  Object.class_eval { const_set configuration_setting, ENV[configuration_setting] }
+end
+
 class Pringle < Sinatra::Base
-  MINGLE_USERNAME = "bguthrie"
-  MINGLE_PASSWORD = "password"
-  MINGLE_HOST     = "localhost:8080"
   MINGLE_API_BASE = "/api/v2"
 
   set :app_file, __FILE__
@@ -20,16 +25,20 @@ class Pringle < Sinatra::Base
   get '/pringle' do
     haml :index
   end
-
+  
+  def base_mingle_uri(path)
+    (MINGLE_HOST + MINGLE_API_BASE + path + ".xml").to_uri(:username => MINGLE_USERNAME, :password => MINGLE_PASSWORD)
+  end
+  
   get '/mingle' do
     content_type "application/json"
-    mingle_uri = "http://#{MINGLE_USERNAME}:#{MINGLE_PASSWORD}@#{MINGLE_HOST}#{MINGLE_API_BASE}#{params[:path]}.xml?#{params[:params]}"
-    response = mingle_uri.to_uri.get
+    mingle_uri = base_mingle_uri(params[:path])
+    response = mingle_uri.get(params[:params])
 
     case response.code.to_i
     when 404
       status 404
-      body ActiveSupport::JSON.encode(:error => "Not Found", :uri => mingle_uri)
+      body ActiveSupport::JSON.encode(:error => "Not Found", :uri => mingle_uri.inspect)
     when 200
       json = ActiveSupport::JSON.encode(response.deserialise)
       if params[:callback]
